7a9402daa26000bdd1c9e96167583bfdabe4eea4
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2025-01-23 21:56:51 +0000

    basic state flipping on Flashcard

--- src/Components/Flashcard/Flashcard.jsx
+++ src/Components/Flashcard/Flashcard.jsx
@@ -1,10 +1,31 @@
 
-export default function Flashcard({value, handleClick}) {
+import { useState } from "react";
+
+let front = ["Q1","Q2","Q3", "Q4", "Q5", "Q6"];
+let back = ["A1","A2","A3", "A4", "A5", "A6"];
+
+export default function Flashcard({value}) {
+
+const [cardFace, setCardFace]= useState("front");
+
+function handleClick() {
+// console.log("Click")
+    if (cardFace === "front") {
+        setCardFace("back")
+        console.log(`back ${value} ${back[value-1]}`)
+    }
+    else {
+        setCardFace("front")
+        console.log(`front ${value} ${front[value-1]}`)
+    }
+
+}
 
     return (
         <button onClick = {handleClick} style= {{ border: "2px solid black", padding: "10px", margin: "10px" }} >
-            {value}
-    </button>
+        {cardFace}
+        {front[value-1]}
+     </button>
     )
     
 }
--- src/Components/FlashcardsContainer/FlashcardsContainer.jsx
+++ src/Components/FlashcardsContainer/FlashcardsContainer.jsx
@@ -5,29 +5,18 @@ import { useState } from "react";
 
 export default function FlashcardsContainer() {
 
-const [cardFace, setCardFace]= useState("front");
-
-function handleClick() {
-    console.log("Click")
-    setCardFace(cardFace === "front" ? "back" : "front");
-        if (cardFace === "back") {
-            return( 
-            console.log("Back"))
-        }
-        console.log("Front")
-}
 
     return (
         <div className="FlashcardsContainer" style={{ border: "2px solid black", padding: "10px", margin: "10px" }}>
             <div>
-                <Flashcard value = "Q1" cardface = {cardFace} handleClick = {handleClick}/>
-                <Flashcard value = "Q2" cardface = {cardFace} handleClick = {handleClick}/>
-                <Flashcard value = "Q3" cardface = {cardFace} handleClick = {handleClick}/>
+                <Flashcard value = {1}/>
+                <Flashcard value = {2}/>
+                <Flashcard value = {3}/>
             </div>
             <div>
-                <Flashcard value = "Q4" cardface = {cardFace} handleClick = {handleClick}/>
-                <Flashcard value = "Q5" cardface = {cardFace} handleClick = {handleClick}/>
-                <Flashcard value = "Q6" cardface = {cardFace} handleClick = {handleClick}/>
+                <Flashcard value = {4}/>
+                <Flashcard value = {5}/>
+                <Flashcard value = {6}/>
             </div>
         </ div>
     )
