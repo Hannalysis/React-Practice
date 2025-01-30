a66471803da7b8a8e97ef15799b208c5fc60b554
Jake Stanier <jakestanier@Jakes-MacBook-Pro.local>
2025-01-30 17:54:47 +0000

    fixed .map function

--- src/Components/Flashcard/Flashcard.jsx
+++ src/Components/Flashcard/Flashcard.jsx
@@ -1,20 +1,20 @@
+/* eslint-disable react/prop-types */
 
 import { useState } from "react";
 import ReactCardFlip from 'react-card-flip';
 import "./Flashcard.css"
 
-let front = ["Why do you capitalize function names in React?",
-    "What is JSX in React?","What is the difference between a class component and a functional component?",
-     "What is a React hook?", "What is state in React?", "What is a prop in React?"];
-let back = ["Capitalized function names distinguish React components from HTML elements, ensuring JSX interprets them as user-defined components.",
-    "JSX is a syntax extension that allows writing HTML-like code in JavaScript, which is transpiled to regular JavaScript by Babel.",
-    "Class components extend React.Component with lifecycle methods, while functional components are simpler and use hooks for state and effects.",
-     "A React hook is a function that enables React features (e.g., state, context, lifecycle methods) in functional components.",
-      "State is an object that holds a component's data and triggers a re-render when updated.",
-       "Props are inputs passed from parent to child components and are read-only in the child component."];
+// let front = ["Why do you capitalize function names in React?",
+//     "What is JSX in React?","What is the difference between a class component and a functional component?",
+//      "What is a React hook?", "What is state in React?", "What is a prop in React?"];
+// let back = ["Capitalized function names distinguish React components from HTML elements, ensuring JSX interprets them as user-defined components.",
+//     "JSX is a syntax extension that allows writing HTML-like code in JavaScript, which is transpiled to regular JavaScript by Babel.",
+//     "Class components extend React.Component with lifecycle methods, while functional components are simpler and use hooks for state and effects.",
+//      "A React hook is a function that enables React features (e.g., state, context, lifecycle methods) in functional components.",
+//       "State is an object that holds a component's data and triggers a re-render when updated.",
+//        "Props are inputs passed from parent to child components and are read-only in the child component."];
 
-// const flashCards = [{front: "front1", back: "back1"}]
-// const flashCards2 = [{card1: {front: "front2", back: "back2"}}]
+
 
 export default function Flashcard({card}) {
 
@@ -31,17 +31,18 @@ function handleClick(e) {
             <button className = "flashcard" 
                     onClick = {handleClick} 
                     style= {{ border: "2px solid black", padding: "10px", margin: "10px"}} >
-                {card.front}
+                {/* // everything is the same apart from this data here and below, we use card.card.front to
+                access the data - the naming convention should be better i agree haha */}
+                {card.card.front}
             </button>
         </div>
         <div>
             <button className = "flashcard" 
                     onClick = {handleClick} 
                     style= {{ border: "2px solid black", padding: "10px", margin: "10px"}} >
-                {card.back}
+                {card.card.back}
             </button>
         </div>
     </ReactCardFlip>
     )
-    
 }
--- src/Components/FlashcardsContainer/FlashcardsContainer.jsx
+++ src/Components/FlashcardsContainer/FlashcardsContainer.jsx
@@ -1,17 +1,43 @@
 import Flashcard from "../Flashcard/Flashcard"
 import "./FlashcardsContainer.css"
-import { useState } from "react";
 
 
 
 export default function FlashcardsContainer() {
-    const flashCards = [{card1: {front: "front2", back: "back2"}}]
+    // this is the array that stores the flashcard info.
+    // you can add as many objects to this array (in the correct format) and they will all be rendered on the page
+    // might also be nice to store these in a different file and import them - keeps it nice and clean :)
+    const flashCards = [
+        {card: 
+            {front: "This is the front of card1", 
+            back: "This is the back of card1"}
+        }, 
+        {card: 
+            {front: "This is the front of card2", 
+            back: "This is the back of card2"}
+        },
+        {card: 
+            {front: "This is the front of card3", 
+            back: "This is the back of card3"}
+        }
+    ]
 
-    flashCards.map((flashcard, index) => {
-        return (
-            <Flashcard card={flashcard}/>
-        )
-    })
+    // this is the return statement for the function "FlashcardsContainer"
+    return(
+        // here we map or "iterate" through the array above
+        // for each iteration or entry/flashcard in the array, we return the <Flashcard /> component with that iterations data
+        flashCards.map((flashcard, index) => {
+            return (
+                // dont worry about the key prop, i added it to get rid of the red error line
+                // the 'flashcard' prop is the current iterations data
+                <Flashcard key={index} card={flashcard}/>
+            )
+        })
+    )
+
+
+
+
 
     // return (
     //     <div className="flashcardsContainer" style={{ border: "2px solid black", padding: "10px", margin: "10px" }}>
